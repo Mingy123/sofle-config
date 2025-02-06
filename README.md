6d1ca3803e26368ac044310f36b99c771e2137a2
Mingy123 <mingy123.coc@gmail.com>
2025-02-07 01:23:03 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # sofle-config
 dotfiles for sofle rgb keyboard
+
+# Installation
+Pull the qmk firmware repository: `git pull https://github.com/qmk/qmk_firmware/`  
+Reset it to a legacy version: `git reset --hard d2892685c52a8a0892b28872cfc43e8f7fa37465`  
+Copy glcdfont.c over to drivers/oled/  
+Flash to keyboard: `qmk flash -kb sofle/rev1 -km <KEYMAP_NAME>`
--- keymap.c
+++ keymap.c
@@ -114,9 +114,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |-------+-----+------+------+------+------|                    |------+------+------+------+------+--------|
  * |  ESC  |  A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |   '    |
  * |-------+-----+------+------+------+------|  VOL  |    |  VOL  |------+------+------+------+------+--------|
- * |  SHFT |  Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |  RAlt  |
+ * |  SHFT |  Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |  RCtl  |
  * `-----------------------------------------/       /     \      \-------------------------------------------'
- *            | MOVE | ALT  |  Win | Space| / CTRL  /       \ ENTER\  | CODE | MOVE |Shift | RCtl |
+ *            | MOVE | ALT  |  Win | Space| / CTRL  /       \ ENTER\  | CODE | MOVE |Shift | Win  |
  *            |      |      |      |      |/       /         \      \ |      |      |      |      |
  *            `-----------------------------------'           '------''---------------------------'
  */
@@ -130,7 +130,7 @@ LT(_NUMPAD,KC_GRV), KC_1, KC_2, KC_3,  KC_4,   KC_5,                         KC_
   //|------+-------+--------+--------+--------+------|  ===  |      |  ===  |--------+-------+--------+--------+--------+---------|
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,  KC_EC11,      KC_MUTE, KC_N,    KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_RCTL,
   //|------+-------+--------+--------+--------+------|  ===  |      |  ===  |--------+-------+--------+--------+--------+---------|
-                 MO(_MOVE), KC_LALT, KC_LGUI,  KC_SPC, KC_LCTL,    KC_ENT,MO(_CODE),MO(_MOVE),KC_RSFT, KC_RALT
+                 MO(_MOVE), KC_LALT, KC_LGUI,  KC_SPC, KC_LCTL,    KC_ENT,MO(_CODE),MO(_MOVE),KC_RSFT, KC_RWIN
   //            \--------+--------+--------+---------+-------|      |--------+---------+--------+---------+-------/
 ),
 
@@ -398,7 +398,7 @@ static void print_status(void) {
 }
 
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
-    return rotation;
+    return OLED_ROTATION_0;
 /*
     if (is_keyboard_master()) {
         return OLED_ROTATION_270;
